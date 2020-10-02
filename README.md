951b53b20e26c77bbd4beab3d2f601b357f5a317
Paul T <paultoro101@gmail.com>
2020-10-01 21:08:08 -0400

    changed READ ME FILE and add some java to main page

--- README.md
+++ README.md
@@ -1,37 +1,7 @@
-## Welcome to GitHub Pages
+## Read Me - Paul I Toro Portfolio
+This portfolio is a cumulation of work done at CUNY Tech Works at QueensBorough Community College and through Columbia Engineering Bootcamp.
+Ive tried to incorporate HTML,CSS, ANd some JAVA Script in my projects. 
 
-You can use the [editor on GitHub](https://github.com/pault929/Portfolio2/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
+I've decided to keep all my projects in the projects page to demonstrate and show my progression.
 
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
-
-### Markdown
-
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
-
-```markdown
-Syntax highlighted code block
-
-# Header 1
-## Header 2
-### Header 3
-
-- Bulleted
-- List
-
-1. Numbered
-2. List
-
-**Bold** and _Italic_ and `Code` text
-
-[Link](url) and ![Image](src)
-```
-
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
-
-### Jekyll Themes
-
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/pault929/Portfolio2/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
-
-### Support or Contact
-
-Having trouble with Pages? Check out our [documentation](https://docs.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
+I really like the web design aspect, in the near future more projects will be added with other technicality of the design and coding.
---
+++ index.js
@@ -0,0 +1,71 @@
+// / var todo = "Orlando";
+
+$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=5128581&APPID=411910c4c4abb733221242b4d25a13f1&units=imperial", function(data){
+
+console.log(data);
+
+// var icon ="http://openweathermap.org/img/wn/10d@2x.png";
+var icon ="http://api.openweathermap.org/data/2.5/forecast?q=5128581&APPID=411910c4c4abb733221242b4d25a13f1&units=imperial";
+
+
+
+
+var date = newDate();
+  document.getElementById("datetime").innerHtml = date.toLocaleString();
+
+});
+
+
+const key = '';
+if(key == '') document.getElementById('temp').innerHTML = ('411910c4c4abb733221242b4d25a13f1');
+
+function weatherBallon( cityID ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + 5128581 + '&appid=' + '411910c4c4abb733221242b4d25a13f1')  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data);
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+  
+  
+  document.getElementById('description2').innerHTML = description;
+	document.getElementById('temp2').innerHTML = celcius + '&deg;';
+	document.getElementById('location2').innerHTML = d.name;
+  
+  
+  document.getElementById('description3').innerHTML = description;
+	document.getElementById('temp3').innerHTML = celcius + '&deg;';
+	document.getElementById('location3').innerHTML = d.name;
+  
+  
+  document.getElementById('description4').innerHTML = description;
+	document.getElementById('temp4').innerHTML = celcius + '&deg;';
+	document.getElementById('location4').innerHTML = d.name;
+  
+  
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
+window.onload = function() {
+	weatherBallon( 6167865 );
+}
+
